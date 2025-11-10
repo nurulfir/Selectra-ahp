34f56fc8e9887e60ec84b0c255dd367442baffab
nurulfir <muhammadnurulfirdaus05@gmail.com>
2025-11-11 05:36:18 +0700

    Selectra

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
-// src/App.jsx
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import AHPAnalysis from './pages/Compare';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import AHPAnalysis from "./pages/Compare";
+import ClickSpark from "./components/ClickSpark";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="min-h-screen bg-gray-50">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/analisis" element={<AHPAnalysis />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<ClickSpark
+				sparkColor="#fff"
+				sparkSize={10}
+				sparkRadius={15}
+				sparkCount={8}
+				duration={400}
+			>
+				<div className="min-h-screen bg-gray-50">
+					<Navbar />
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/analisis" element={<AHPAnalysis />} />
+					</Routes>
+				</div>
+			</ClickSpark>
+		</BrowserRouter>
+	);
 }
 
 export default App;
